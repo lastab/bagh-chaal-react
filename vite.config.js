b90676d3a9c5239786475cae00b76adbb0d7edc5
Batsal Awaley <batsal.awaley@gmail.com>
2026-07-19 20:27:44 +1000

    Fix vite config js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react':    ['react', 'react-dom'],
+          'vendor-firebase': ['firebase/app', 'firebase/database', 'firebase/auth'],
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     VitePWA({
